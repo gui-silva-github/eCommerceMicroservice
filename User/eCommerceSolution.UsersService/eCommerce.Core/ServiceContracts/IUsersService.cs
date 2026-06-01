7996c6ca673f4c470680dfaba71914db1178c8fc
Guilherme Silva <guilhermeviniciusbispodasilva@gmail.com>
2026-06-01 14:14:30 -0300

    feat: users service (interface)

---
+++ User/eCommerceSolution.UsersService/eCommerce.Core/ServiceContracts/IUsersService.cs
@@ -0,0 +1,11 @@
+﻿using eCommerce.Core.DTO;
+
+namespace eCommerce.Core.ServiceContracts
+{
+    public interface IUsersService
+    {
+        Task<AuthenticationResponse> Login(LoginRequest loginRequest);
+
+        Task<AuthenticationResponse> Register(RegisterRequest registerRequest);
+    }
+}
