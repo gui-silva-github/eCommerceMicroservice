6a947b5bb2a3621a94313a1c8cc37fea90bb5a14
Guilherme Silva <guilhermeviniciusbispodasilva@gmail.com>
2026-06-01 14:17:00 -0300

    feat: login request validator

---
+++ User/eCommerceSolution.UsersService/eCommerce.Core/Validators/LoginRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using eCommerce.Core.DTO;
+using FluentValidation;
+
+namespace eCommerce.Core.Validators
+{
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(temp => temp.Email)
+                .NotEmpty().WithMessage("Email é obrigatório.")
+                .EmailAddress().WithMessage("Formato de email inválido.");
+
+            RuleFor(temp => temp.Password)
+                .NotEmpty().WithMessage("Senha é obrigatória.");
+        }
+    }
+}
