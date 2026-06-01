901331b544d28a1572d808fcead38e610709c33a
Guilherme Silva <guilhermeviniciusbispodasilva@gmail.com>
2026-06-01 14:07:00 -0300

    feat: DI for 'eCommerce.Core'

---
+++ User/eCommerceSolution.UsersService/eCommerce.Core/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using eCommerce.Core.Mappers;
+using eCommerce.Core.ServiceContracts;
+using eCommerce.Core.Services;
+using eCommerce.Core.Validators;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.Core
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddCore(this IServiceCollection services)
+        {
+            services.AddAutoMapper(cfg =>
+            {
+                cfg.AddProfile<ApplicationUserMappingProfile>();
+                cfg.AddProfile<RegisterRequestMappingProfile>();
+            });
+
+            services.AddTransient<IUsersService, UsersService>();
+            services.AddValidatorsFromAssemblyContaining<LoginRequestValidator>();
+
+            return services;
+        }
+    }
+}
