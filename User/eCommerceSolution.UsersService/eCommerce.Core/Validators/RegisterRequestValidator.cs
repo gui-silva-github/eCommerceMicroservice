70e637ec9a64a42ebc776c4f1f7fa38c26c2c801
Guilherme Silva <guilhermeviniciusbispodasilva@gmail.com>
2026-06-01 14:17:29 -0300

    feat: register request validator

---
+++ User/eCommerceSolution.UsersService/eCommerce.Core/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,25 @@
+﻿using eCommerce.Core.DTO;
+using FluentValidation;
+
+namespace eCommerce.Core.Validators
+{
+    public class RegisterRequestValidator : AbstractValidator<RegisterRequest> 
+    {
+        public RegisterRequestValidator()
+        {
+            RuleFor(temp => temp.Email)
+                .NotEmpty().WithMessage("Email é obrigatório.")
+                .EmailAddress().WithMessage("Formato de email inválido.");
+
+            RuleFor(temp => temp.Password)
+                .NotEmpty().WithMessage("Senha é obrigatória.");
+
+            RuleFor(request => request.PersonName)
+                .NotEmpty().WithMessage("O nome da pessoa não pode ser vazio.")
+                .Length(1, 50).WithMessage("O nome da pessoa deve possuir de 1 a 50 caracteres.");
+
+            RuleFor(request => request.Gender)
+                .IsInEnum().WithMessage("Gênero inválido.");
+        }
+    }
+}
