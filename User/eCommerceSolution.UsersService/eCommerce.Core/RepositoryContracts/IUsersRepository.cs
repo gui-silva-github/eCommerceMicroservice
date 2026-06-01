040f2375d6e208db5715d57096ba500da3f70542
Guilherme Silva <guilhermeviniciusbispodasilva@gmail.com>
2026-06-01 14:14:11 -0300

    feat: users repository (interface)

---
+++ User/eCommerceSolution.UsersService/eCommerce.Core/RepositoryContracts/IUsersRepository.cs
@@ -0,0 +1,13 @@
+﻿using eCommerce.Core.Entities;
+
+namespace eCommerce.Core.RepositoryContracts
+{
+    public interface IUsersRepository
+    {
+        Task<ApplicationUser> AddUser(ApplicationUser user);
+
+        Task<ApplicationUser?> GetUserByEmail(string email);
+
+        Task<ApplicationUser?> GetUserByEmailAndPassword(string? email, string? password);
+    }
+}
