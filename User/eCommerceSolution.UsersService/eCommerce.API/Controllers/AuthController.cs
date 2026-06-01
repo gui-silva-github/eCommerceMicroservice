1ed76d32dc859e345a0db17a3cd9bd172f16b6d3
Guilherme Silva <guilhermeviniciusbispodasilva@gmail.com>
2026-06-01 13:53:29 -0300

    feat: add auth controller for application

---
+++ User/eCommerceSolution.UsersService/eCommerce.API/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using eCommerce.Core.DTO;
+using eCommerce.Core.ServiceContracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUsersService _usersService;
+
+        public AuthController(IUsersService usersService)
+        {
+            _usersService = usersService;
+        }
+
+        [HttpPost("register")]
+        [ProducesResponseType(typeof(AuthenticationResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status409Conflict)]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest registerRequest)
+        {
+            AuthenticationResponse authenticationResponse = await _usersService.Register(registerRequest);
+            return Ok(authenticationResponse);
+        }
+
+        [HttpPost("login")]
+        [ProducesResponseType(typeof(AuthenticationResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+        {
+            AuthenticationResponse authenticationResponse = await _usersService.Login(loginRequest);
+            return Ok(authenticationResponse);
+        }
+    }
+}
