17947b865016b58d6b553aeb671964f893cccc3b
Guilherme Silva <guilhermeviniciusbispodasilva@gmail.com>
2026-06-06 16:54:59 -0300

    feat: product service

---
+++ Product/eCommerceSolution.ProductsService/BusinessLogicLayer/Services/ProductsService.cs
@@ -0,0 +1,120 @@
+﻿using AutoMapper;
+using eCommerce.BusinessLogicLayer.DTO;
+using eCommerce.BusinessLogicLayer.Exceptions;
+using eCommerce.BusinessLogicLayer.ServiceContracts;
+using eCommerce.DataAccessLayer.Entities;
+using eCommerce.DataAccessLayer.RepositoryContracts;
+using FluentValidation;
+using FluentValidation.Results;
+using System.Linq.Expressions;
+
+namespace eCommerce.BusinessLogicLayer.Services
+{
+    public class ProductsService : IProductsService
+    {
+        private readonly IValidator<ProductAddRequest> _productAddRequestValidator;
+        private readonly IValidator<ProductUpdateRequest> _productUpdateRequestValidator;
+        private readonly IMapper _mapper;
+        private readonly IProductsRepository _productsRepository;
+
+        public ProductsService(IValidator<ProductAddRequest> productAddRequestValidator, IValidator<ProductUpdateRequest> productUpdateRequestValidator, IMapper mapper, IProductsRepository productsRepository)
+        {
+            _productAddRequestValidator = productAddRequestValidator;
+            _productUpdateRequestValidator = productUpdateRequestValidator;
+            _mapper = mapper;
+            _productsRepository = productsRepository;
+        }
+
+        public async Task<ProductResponse?> AddProduct(ProductAddRequest productAddRequest)
+        {
+            if (productAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(productAddRequest));
+            }
+
+            await ValidateAsync(_productAddRequestValidator, productAddRequest);
+
+            Product productInput = _mapper.Map<Product>(productAddRequest);
+            Product? addedProduct = await _productsRepository.AddProduct(productInput);
+
+            if (addedProduct == null)
+            {
+                return null;
+            }
+
+            ProductResponse addedProductResponse = _mapper.Map<ProductResponse>(addedProduct);
+            return addedProductResponse;
+        }
+
+        public async Task<bool> DeleteProduct(Guid productID)
+        {
+            Product? existingProduct = await _productsRepository.GetProductByCondition(temp => temp.ProductID == productID);
+
+            if (existingProduct == null)
+            {
+                return false;
+            }
+
+            bool isDeleted = await _productsRepository.DeleteProduct(productID);
+            return isDeleted;
+        }
+
+        public async Task<ProductResponse?> GetProductByCondition(Expression<Func<Product, bool>> conditionExpression)
+        {
+            Product? product = await _productsRepository.GetProductByCondition(conditionExpression);
+            if (product == null)
+            {
+                return null;
+            }
+
+            ProductResponse productResponse = _mapper.Map<ProductResponse>(product);
+            return productResponse;
+        }
+
+        public async Task<List<ProductResponse?>> GetProducts()
+        {
+            IEnumerable<Product?> products = await _productsRepository.GetProducts();
+
+            IEnumerable<ProductResponse?> productResponses = _mapper.Map<IEnumerable<ProductResponse>>(products);
+            return productResponses.ToList();
+        }
+
+        public async Task<List<ProductResponse?>> GetProductsByCondition(Expression<Func<Product, bool>> conditionExpression)
+        {
+            IEnumerable<Product?> products = await _productsRepository.GetProductsByCondition(conditionExpression);
+
+            IEnumerable<ProductResponse?> productResponses = _mapper.Map<IEnumerable<ProductResponse?>>(products);
+            return productResponses.ToList();
+        }
+
+        public async Task<ProductResponse?> UpdateProduct(ProductUpdateRequest productUpdateRequest)
+        {
+            Product? existingProduct = await _productsRepository.GetProductByCondition(temp => temp.ProductID == productUpdateRequest.ProductID);
+
+            if (existingProduct == null)
+            {
+                throw new BusinessException("ID do produto inválido.");
+            }
+
+            await ValidateAsync(_productUpdateRequestValidator, productUpdateRequest);
+
+            Product product = _mapper.Map<Product>(productUpdateRequest);
+
+            Product? updatedProduct = await _productsRepository.UpdateProduct(product);
+
+            ProductResponse? updatedProductResponse = _mapper.Map<ProductResponse>(updatedProduct);
+
+            return updatedProductResponse;
+        }
+
+        private static async Task ValidateAsync<T>(IValidator<T> validator, T instance)
+        {
+            ValidationResult validationResult = await validator.ValidateAsync(instance);
+
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult.Errors);
+            }
+        }
+    }
+}
