37feae9053f2c456acba28d80548ce3d83a71b30
Guilherme Silva <guilhermeviniciusbispodasilva@gmail.com>
2026-06-06 16:58:05 -0300

    feat: main API with CORS + Swagger

---
+++ Product/eCommerceSolution.ProductsService/ProductsMicroService.API/Program.cs
@@ -0,0 +1,80 @@
+﻿using eCommerce.BusinessLogicLayer.DTO;
+using eCommerce.DataAccessLayer.Context;
+using eCommerce.ProductsMicroService.API.APIEndpoints;
+using eCommerce.ProductsMicroService.API.Middleware;
+using eCommerce.ProductsService.BusinessLogicLayer;
+using eCommerce.ProductsService.DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDataAccessLayer(builder.Configuration);
+builder.Services.AddBusinessLogicLayer();
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    })
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            var errors = context.ModelState
+                .Where(entry => entry.Value?.Errors.Count > 0)
+                .ToDictionary(
+                    entry => entry.Key,
+                    entry => entry.Value!.Errors.Select(error => error.ErrorMessage).ToArray());
+
+            var response = new ApiErrorResponse("Dados da requisição inválidos.", errors, "ModelBinding");
+
+            return new BadRequestObjectResult(response);
+        };
+    });
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.EnsureCreated();
+}
+
+app.UseExceptionHandlingMiddleware();
+
+app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapProductAPIEndpoints();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapGet("/", () => Results.Redirect("/swagger")).ExcludeFromDescription();
+}
+
+app.Run();
