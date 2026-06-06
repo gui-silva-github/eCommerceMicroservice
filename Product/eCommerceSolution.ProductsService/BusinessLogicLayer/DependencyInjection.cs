99300f97768bd98acb373fe69d7718ae84327569
Guilherme Silva <guilhermeviniciusbispodasilva@gmail.com>
2026-06-06 16:47:37 -0300

    feat: dependency injection for 'layered architecture'

---
+++ Product/eCommerceSolution.ProductsService/BusinessLogicLayer/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using eCommerce.BusinessLogicLayer.Mappers;
+using eCommerce.BusinessLogicLayer.ServiceContracts;
+using Microsoft.Extensions.DependencyInjection;
+using eCommerce.BusinessLogicLayer.Validators;
+using FluentValidation;
+
+namespace eCommerce.ProductsService.BusinessLogicLayer
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddBusinessLogicLayer(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(ProductAddRequestToProductMappingProfile).Assembly);
+
+            services.AddValidatorsFromAssemblyContaining<ProductAddRequestValidator>();
+
+            services.AddScoped<IProductsService, eCommerce.BusinessLogicLayer.Services.ProductsService>();
+
+            return services;
+        }
+    }
+}
