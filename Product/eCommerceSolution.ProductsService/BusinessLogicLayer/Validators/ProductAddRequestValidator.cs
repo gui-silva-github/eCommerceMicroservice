a7e527e98cc3f56db479d2cc27807b0847abc19c
Guilherme Silva <guilhermeviniciusbispodasilva@gmail.com>
2026-06-06 16:55:29 -0300

    feat: validators for POST and PUT

---
+++ Product/eCommerceSolution.ProductsService/BusinessLogicLayer/Validators/ProductAddRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using eCommerce.BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace eCommerce.BusinessLogicLayer.Validators
+{
+    public class ProductAddRequestValidator : AbstractValidator<ProductAddRequest>
+    {
+        public ProductAddRequestValidator()
+        {
+            RuleFor(temp => temp.ProductName)
+                .NotEmpty().WithMessage("Nome não pode ser vazio");
+            RuleFor(temp => temp.Category)
+                .NotNull().WithMessage("Categoria não pode ser vazia")
+                .IsInEnum().WithMessage("Categoria inválida");
+            RuleFor(temp => temp.UnitPrice)
+                .InclusiveBetween(0, double.MaxValue).WithMessage($"Preço Unitário deve estar entre 0 a {double.MaxValue}");
+            RuleFor(temp => temp.QuantityInStock)
+                .InclusiveBetween(0, int.MaxValue).WithMessage($"Quantidade em Estoque deve estar entre 0 a {int.MaxValue}");
+        }
+    }
+}
---
+++ Product/eCommerceSolution.ProductsService/BusinessLogicLayer/Validators/ProductUpdateRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using eCommerce.BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace eCommerce.BusinessLogicLayer.Validators
+{
+    public class ProductUpdateRequestValidator : AbstractValidator<ProductUpdateRequest>
+    {
+        public ProductUpdateRequestValidator()
+        {
+            RuleFor(temp => temp.ProductID)
+                .NotEmpty().WithMessage("O ID do produto não deve ser vazio");
+            RuleFor(temp => temp.ProductName)
+                .NotEmpty().WithMessage("O nome não pode ser vazio");
+            RuleFor(temp => temp.Category)
+                .NotNull().WithMessage("A categoria não deve ser vazia")
+                .IsInEnum().WithMessage("Categoria inválida");
+            RuleFor(temp => temp.UnitPrice)
+                .InclusiveBetween(0, double.MaxValue).WithMessage($"Preço Unitário deve estar entre 0 a {double.MaxValue}");
+            RuleFor(temp => temp.QuantityInStock)
+                .InclusiveBetween(0, int.MaxValue).WithMessage($"Quantity in Stock should between 0 to {int.MaxValue}");
+        }
+    }
+}
