18b201158ef6dd89ee7760eae8117cc60c4d0db0
Guilherme Silva <guilhermeviniciusbispodasilva@gmail.com>
2026-06-06 16:50:16 -0300

    feat: business exception

---
+++ Product/eCommerceSolution.ProductsService/BusinessLogicLayer/Exceptions/BusinessException.cs
@@ -0,0 +1,9 @@
+namespace eCommerce.BusinessLogicLayer.Exceptions
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException(string message) : base(message)
+        {
+        }
+    }
+}
