9c59b26cb8cc81f28efa7c10e4a123aa8076c961
Guilherme Silva <guilhermeviniciusbispodasilva@gmail.com>
2026-06-06 16:56:07 -0300

    feat: Product entity

---
+++ Product/eCommerceSolution.ProductsService/DataAccessLayer/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.DataAccessLayer.Entities
+{
+    public class Product
+    {
+        [Key]
+        public Guid ProductID { get; set; }
+        public required string ProductName { get; set; }
+        public required string Category { get; set; }
+        public double? UnitPrice { get; set; }
+        public int? QuantityInStock { get; set; }
+    }
+}
