3879a117fc195eb87f5ae00365d5c322600a8e25
Guilherme Silva <guilhermeviniciusbispodasilva@gmail.com>
2026-07-05 02:00:07 -0300

    feat: launchsettings and DI for DAL and BLL

---
+++ Order/eCommerceSolution.OrdersService/BusinessLogicLayer/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using eCommerce.OrdersMicroservice.BusinessLogicLayer.Mappers;
+using eCommerce.OrdersMicroservice.BusinessLogicLayer.ServiceContracts;
+using eCommerce.OrdersMicroservice.BusinessLogicLayer.Services;
+using eCommerce.OrdersMicroservice.BusinessLogicLayer.Validators;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.OrdersMicroservice.BusinessLogicLayer
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddBusinessLogicLayer(this IServiceCollection services)
+        {
+            services.AddAutoMapper(cfg =>
+            {
+                cfg.AddMaps(typeof(OrderAddRequestToOrderMappingProfile).Assembly);
+            });
+
+            services.AddValidatorsFromAssemblyContaining<OrderAddRequestValidator>();
+
+            services.AddScoped<IOrdersService, OrdersService>();
+
+            return services;
+        }
+    }
+}
---
+++ Order/eCommerceSolution.OrdersService/DataAccessLayer/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using eCommerce.OrdersMicroservice.DataAccessLayer.RepositoryContracts;
+using eCommerce.OrdersMicroservice.DataAccessLayer.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace eCommerce.OrdersMicroservice.DataAccessLayer
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionStringTemplate = configuration.GetConnectionString("MongoDB")!;
+            string connectionString = connectionStringTemplate
+                .Replace("$MONGO_HOST", Environment.GetEnvironmentVariable("MONGODB_HOST") ?? "localhost")
+                .Replace("$MONGO_PORT", Environment.GetEnvironmentVariable("MONGODB_PORT") ?? "27017");
+
+            services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
+
+            services.AddScoped<IMongoDatabase>(provider =>
+            {
+                IMongoClient client = provider.GetRequiredService<IMongoClient>();
+                return client.GetDatabase("OrdersDatabase");
+            });
+
+            services.AddScoped<IOrdersRepository, OrdersRepository>();
+
+            return services;
+        }
+    }
+}
